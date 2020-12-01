c677c7c80b39d2260d255069cd0b1db5998b5627
Khodr <khodr@umich.edu>
2020-11-30 19:00:23 -0500

    adding empty tests to master

---
+++ testSelectionSort.java
@@ -0,0 +1,34 @@
+package lab10;
+// KHODR SALMAN - CIS 285 - LAB 10
+import org.junit.Test;
+
+public class testSelectionSort {
+
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+
+    public testSelectionSort() {
+    }
+
+    public void testPositive(){ //testing a list of all positive integers
+
+    }
+
+    public void testNegative(){ // testing a list of all negative integers
+
+    }
+
+
+    public void testMixed(){ //testing a list containing positive, negative and zeros.
+
+    }
+
+    public void testDuplicates(){
+    //testing a list containing one or more duplicate number, for both positive and negative numbers
+
+}
